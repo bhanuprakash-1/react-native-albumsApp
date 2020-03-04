43bda2824bb69ff2f96257914abde7f80e26b2b5
bhanuprakash-1 <chinthalapallybhanu@gmail.com>
2020-03-04 22:33:18 +0530

    AlbumList renderAlbums() added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,26 @@
 import React , {Component} from 'react';
 import { View , Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state = {albums:[] };
 
-    componentWillMount(){
-        console.log("This log is from componentWillMount ");
+    UNSAFE_componentWillMount(){
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response =>this.setState({albums: response.data }) );
     }
+
+    renderAlbums(){
+        namesList = this.state.albums.map(album => <Text>{album.title}</Text>); 
+        return namesList;
+    }
+
     render(){
+        console.log(this.state);
         return (
             <View>
-                <Text> Album List 12456</Text>
-
+                {/* <Text> Album List 124567</Text>  */}
+                {this.renderAlbums()}
             </View> 
 
 
